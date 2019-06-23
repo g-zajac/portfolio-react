b45c68a38ae10fbb04baef43a7edd9cb775817d0
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-23 22:25:01 +0200

    test plx (working)

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,20 +1,38 @@
 import React from "react";
+import Plx from "react-plx";
+
+const parallaxData = [
+  {
+    start: 0,
+    end: 800,
+    easing: "ease",
+    properties: [
+      {
+        startValue: 0,
+        endValue: -200,
+        property: "translateY"
+      }
+    ]
+  }
+];
 
 export function Welcome() {
   return (
-    <div id="welcome">
-      <p id="intro_text">
-        Hi, I am Grzegorz Zajac, Berlin based passionate engineer,
-        multidiscipline theatre technician, maker and problem solver. I provide
-        innovative embedded electronic solutions for theatre magic.
-      </p>
-      <div id="overlay" />
-      <img
-        id="background_image"
-        src="images/esp8266-cover-1920x1080.jpg"
-        alt=""
-      />
-    </div>
+    <Plx className="MyAwesomeParallax" parallaxData={parallaxData}>
+      <div id="welcome">
+        <p id="intro_text">
+          Hi, I am Grzegorz Zajac, Berlin based passionate engineer,
+          multidiscipline theatre technician, maker and problem solver. I
+          provide innovative embedded electronic solutions for theatre magic.
+        </p>
+        <div id="overlay_welcome" />
+        <img
+          id="background_image"
+          src="images/esp8266-cover-1920x1080.jpg"
+          alt=""
+        />
+      </div>
+    </Plx>
   );
 }
 
