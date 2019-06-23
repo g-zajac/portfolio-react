479bafc7f5c07ce8f090d44b7334525615b0c8a1
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-23 19:13:30 +0200

    add proejct data

---
+++ src/data/portfolioData.js
@@ -0,0 +1,20 @@
+let portfolioData = {
+  projects: [
+    {
+      title: "Siegfried",
+      short_description:
+        "Sparks and fire flames special effect for Siegfried opera.",
+      minature_image_url: "/images/siegfried-cover-1920x1080.jpg",
+      minature_image_tag: "Siegfried - sepcial effect"
+    },
+    {
+      title: "Twelve",
+      short_description:
+        "Contemporary dance performance with with remotely controlled LEDs in costumes.",
+      minature_image_url: "/images/12_minature_image.jpg",
+      minature_image_tag: "Led Costumes"
+    }
+  ]
+};
+
+export default portfolioData;
