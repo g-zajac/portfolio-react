4cb2654dfe42936dc7853e576bf72e5693fc2180
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-23 22:05:19 +0200

    no parallax

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import { Welcome } from "./components/welcome";
 import { Portfolio } from "./components/portfolio";
 import { Contact } from "./components/contact";
 import portfolioData from "./data/portfolioData.js";
-import { ParallaxProvider } from "react-scroll-parallax";
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <ParallaxProvider>
-          <Welcome />
-        </ParallaxProvider>
+        <Welcome />
         <Portfolio portfolioData={portfolioData.projects} />
         <Contact />
       </div>
