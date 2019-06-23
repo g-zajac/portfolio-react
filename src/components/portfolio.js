1c580f243b7b3c4cb39da660caa5575de61a1694
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-23 17:06:47 +0200

    add grid structure

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -3,7 +3,12 @@ import React from "react";
 export function Portfolio() {
   return (
     <div id="portfolio">
-      <p>portfolio</p>
+      <div className="portfolio_grid_wrapper">
+        <div class="box a">A</div>
+        <div class="box b">B</div>
+        <div class="box c">C</div>
+        <div class="box d">D</div>
+      </div>
     </div>
   );
 }
