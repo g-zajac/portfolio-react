a9f6cec5c175a3f85bc2e32f45110aff79a21940
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-23 20:40:39 +0200

    test react-scroll-parallax

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Welcome } from "./components/welcome";
 import { Portfolio } from "./components/portfolio";
 import { Contact } from "./components/contact";
 import portfolioData from "./data/portfolioData.js";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Welcome />
-
+        <ParallaxProvider>
+          <Welcome />
+        </ParallaxProvider>
         <Portfolio portfolioData={portfolioData.projects} />
         <Contact />
       </div>
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { Parallax } from "react-parallax";
 
 export function Welcome() {
   return (
-    <Parallax bgImage={"images/esp8266-cover-1920x1080.jpg"} strength={500}>
-      <div id="welcome">
-        <p id="intro_text">
-          Hi, I am Grzegorz Zajac, Berlin based passionate engineer,
-          multidiscipline theatre technician, maker and problem solver. I
-          provide innovative embedded electronic solutions for theatre magic.
-        </p>
-      </div>
-    </Parallax>
+    <div id="welcome">
+      <p id="intro_text">
+        Hi, I am Grzegorz Zajac, Berlin based passionate engineer,
+        multidiscipline theatre technician, maker and problem solver. I provide
+        innovative embedded electronic solutions for theatre magic.
+      </p>
+      <div id="overlay" />
+      <img
+        id="background_image"
+        src="images/esp8266-cover-1920x1080.jpg"
+        alt=""
+      />
+    </div>
   );
 }
 
