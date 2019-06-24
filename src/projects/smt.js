d33474bd60c751277a743bddbaf658a2e12fcfb4
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-24 14:49:19 +0200

    add background image

--- src/projects/smt.js
+++ src/projects/smt.js
@@ -4,6 +4,11 @@ export function Smt() {
   return (
     <div className="project">
       <p>Project component test - smt</p>
+      <img
+        className="project_background_image"
+        src="../images/smt_cover.jpg"
+        alt=""
+      />
     </div>
   );
 }
