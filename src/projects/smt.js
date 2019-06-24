49d0b6f7c4e9e541ae1f1580151af4f02f427e55
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-24 19:22:24 +0200

    add more smt project content

--- src/projects/smt.js
+++ src/projects/smt.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export function Smt() {
   return (
-    <div className="project smt">
+    <div className="project" id="smt">
       <div className="project_header">
-        <h3>Shiver Me Timbers</h3>
+        <h2>Shiver Me Timbers</h2>
         <h4>Multimedia Server for a stormy sea installation.</h4>
-        <p>Artistic Director, Choreographer: Rachel Jonhson Experiential</p>
+        <p>Artistic Director, Choreographer: Rachel Jonhson, Experiential</p>
         <p>Scenography & Lighitng Design: Shizuka Hariu</p>
         <p>The Word, South Shields, 2017</p>
       </div>
@@ -19,6 +19,7 @@ export function Smt() {
             The Pod
           </p>
         </div>
+
         <div className="project_challenge">
           <h3>CHALLANGE</h3>
           <p>
@@ -26,12 +27,21 @@ export function Smt() {
             lights, sound, motion sensor.
           </p>
         </div>
+
         <div className="project_solution">
           <h3>SOLUTION</h3>
           <p>
             I designed, programmed and built a mini multimedia server based on
             Raspberry Pi 3 board with UPS board and Enttec USB2DMX modules. The
-            server were controlling lights, sound and sensor.
+            server were controlling lights, sound and sensor. I used minimal
+            headless version of Debian operating system with QLC+ and Node-Red
+            applications installed. QLC+ run lights sequence and playback sound
+            track. The Node-Red controlls sensor, triggering lights cues, sound
+            cues, schedule, system monitoring, diagnostic MQTT messaging etc.
+            QLC+ was linked with Node-red over OSC. The server run the
+            exhibition for over half a year faultless. The MQTT messagaes were
+            subscribed and stored in a cloud database. Counter and visit chart
+            are generated with grafana from stored data.
           </p>
         </div>
       </div>
@@ -43,3 +53,9 @@ export function Smt() {
     </div>
   );
 }
+
+// <img
+//   className="project_background_image"
+//   src="../images/smt_cover.jpg"
+//   alt=""
+// />
