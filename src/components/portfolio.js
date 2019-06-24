3828417f4766b830a0e065b4b8e1d33a8f71f623
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-24 11:13:47 +0200

    render thumbnails with map

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -10,66 +10,32 @@ import { Hoyh } from "../projects/hoyh";
 
 export class Portfolio extends React.Component {
   render() {
-    let projectData = this.props.portfolioData;
-
+    console.log("portfoli data: ", this.props.portfolioData);
     return (
       <div id="portfolio">
         <BrowserRouter>
           <div className="portfolio_grid_wrapper">
-            <div className="box a">
-              <Link to={projectData[0].component}>
-                <PortfolioThumbnail data={projectData[0]} />
-              </Link>
-            </div>
-            <div className="box b">
-              <Link to={projectData[1].component}>
-                <PortfolioThumbnail data={projectData[1]} />
-              </Link>
-            </div>
-            <div className="box c">
-              <Link to={projectData[2].component}>
-                <PortfolioThumbnail data={projectData[2]} />
-              </Link>
-            </div>
-            <div className="box d">
-              <Link to={projectData[3].component}>
-                <PortfolioThumbnail data={projectData[3]} />
-              </Link>
-            </div>
-            <div className="box e">
-              <Link to={projectData[4].component}>
-                <PortfolioThumbnail data={projectData[4]} />
-              </Link>
-            </div>
-            <div className="box f">
-              <Link to={projectData[5].component}>
-                <PortfolioThumbnail data={projectData[5]} />
-              </Link>
-            </div>
+            {this.props.portfolioData &&
+              this.props.portfolioData.map(project => (
+                <div className="box" key={project.id}>
+                  <Link to={`/${project.component}`}>
+                    <PortfolioThumbnail data={project} />
+                  </Link>
+                </div>
+              ))}
           </div>
-
-          <Route exact path="/:id" component={Child} />
-
-          <Route exact path="/siegfried" render={() => <Siegfried />} />
-          <Route exact path="/twelve" render={() => <Twelve />} />
-          <Route exact path="/trapped" render={() => <Trapped />} />
-          <Route exact path="/smt" render={() => <Smt />} />
-          <Route exact path="/strangers" render={() => <Strangers />} />
-          <Route exact path="/hoyh" render={() => <Hoyh />} />
+          <Route exact path="/:id" component={Comp} />
         </BrowserRouter>
       </div>
     );
   }
 }
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function Child({ match }) {
+function Comp({ match }) {
+  console.log("Match: ", match);
   return (
     <div>
-      <h3>ID: {capitalizeFirstLetter(match.params.id)}</h3>
+      <h3>ID: {match.params.id}</h3>
     </div>
   );
 }
