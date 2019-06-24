5909214022ce9fdbcd6ab611025996f450d230f9
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-24 11:21:06 +0200

    move routing to App

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,41 +1,31 @@
 import React from "react";
 import PortfolioThumbnail from "./thumbnails";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import { Siegfried } from "../projects/siegfried";
-import { Twelve } from "../projects/twelve";
-import { Trapped } from "../projects/trapped";
-import { Smt } from "../projects/smt";
-import { Strangers } from "../projects/strangers";
-import { Hoyh } from "../projects/hoyh";
 
 export class Portfolio extends React.Component {
   render() {
-    console.log("portfoli data: ", this.props.portfolioData);
     return (
       <div id="portfolio">
-        <BrowserRouter>
-          <div className="portfolio_grid_wrapper">
-            {this.props.portfolioData &&
-              this.props.portfolioData.map(project => (
-                <div className="box" key={project.id}>
-                  <Link to={`/${project.component}`}>
-                    <PortfolioThumbnail data={project} />
-                  </Link>
-                </div>
-              ))}
-          </div>
-          <Route exact path="/:id" component={Comp} />
-        </BrowserRouter>
+        <div className="portfolio_grid_wrapper">
+          {this.props.portfolioData &&
+            this.props.portfolioData.map(project => (
+              <div className="box" key={project.id}>
+                <PortfolioThumbnail data={project} />
+              </div>
+            ))}
+        </div>
       </div>
     );
   }
 }
 
-function Comp({ match }) {
-  console.log("Match: ", match);
-  return (
-    <div>
-      <h3>ID: {match.params.id}</h3>
-    </div>
-  );
-}
+// <Link to={`/${project.component}`} />;
+// <Route exact path="/:id" component={Comp} />;
+//
+// function Comp({ match }) {
+//   console.log("Match: ", match);
+//   return (
+//     <div>
+//       <h3>ID: {match.params.id}</h3>
+//     </div>
+//   );
+// }
