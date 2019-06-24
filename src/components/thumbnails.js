6830c2375d3cbf01bc05f0d269cd2a382b6bf6c5
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-24 19:22:02 +0200

    remove redundunt action link

--- src/components/thumbnails.js
+++ src/components/thumbnails.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { showProject } from "../actions";
 import { Link } from "react-router-dom";
 
 class PortfolioThumbnail extends React.Component {
