f64786933ba1e79160a21bb6666ed5522f2bc3c6
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-25 12:34:22 +0200

    add socket proxy

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,12 +2,16 @@ import * as io from "socket.io-client";
 
 import { toggleSwitch, slider } from "./actions";
 
+import socketIOClient from "socket.io-client";
+let proxyServer = "http://127.0.0.1:5000";
 export let socket;
 
 export const init = store => {
   if (!socket) {
-    socket = io.connect();
+    socket = socketIOClient(proxyServer);
+    // socket = io.connect();
     socket.on("chatMessages", msgs => store.dispatch(toggleSwitch(msgs)));
     socket.on("chatMessage", msg => store.dispatch(slider(msg)));
   }
+  return socket;
 };
