4cded5e692fdb79a7e1f97a7dc412af8a384d87a
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-25 11:23:00 +0200

    basic server test

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/test", (req, res) => {
+    const test = [{ id: 1, name: "jas" }, { id: 2, name: "fasola" }];
+    res.json(test);
+});
+
+app.listen(port, () => console.log("Server started on port ${port}"));
