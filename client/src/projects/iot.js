250bfe7f685ce318431c3c98e0da01963f07f356
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-25 13:51:36 +0200

    add socket io sends

--- client/src/projects/iot.js
+++ client/src/projects/iot.js
@@ -2,7 +2,7 @@ import React from "react";
 import Switch from "@material-ui/core/Switch";
 import Slider from "@material-ui/lab/Slider";
 import { makeStyles } from "@material-ui/core/styles";
-
+import { socket } from "../socket";
 const useStyles = makeStyles({
   root: {
     width: 200
@@ -12,17 +12,19 @@ const useStyles = makeStyles({
 export function Iot() {
   const classes = useStyles();
   const [state, setState] = React.useState({
-    checkedA: true,
     checkedB: true
   });
 
-  const handleChange = name => event => {
-    console.log("slider value: ");
+  const toggleHandleChange = name => event => {
+    console.log("toggle value: ", event.target.checked);
+    socket.emit("toggleSwitch", event.target.checked);
     setState({ ...state, [name]: event.target.checked });
   };
 
   const [value, setValue] = React.useState(30);
   const sliderHandleChange = (event, newValue) => {
+    console.log("slider value: ", newValue);
+    socket.emit("sliderValue", newValue);
     setValue(newValue);
   };
 
@@ -32,13 +34,14 @@ export function Iot() {
         Lamp
         <Switch
           checked={state.checkedB}
-          onChange={handleChange("checkedB")}
+          onChange={toggleHandleChange("checkedB")}
           value="checkedB"
           color="primary"
           inputProps={{ "aria-label": "primary checkbox" }}
         />
         <Slider
           defaultValue={30}
+          onChange={sliderHandleChange}
           getAriaValueText={valuetext}
           aria-labelledby="continuous-slider"
           valueLabelDisplay="auto"
