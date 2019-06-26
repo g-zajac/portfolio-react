0f601641e994277997e731267eae371774b44597
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-26 23:09:27 +0200

    add title to portfolio

--- client/src/components/portfolio.js
+++ client/src/components/portfolio.js
@@ -5,6 +5,12 @@ export class Portfolio extends React.Component {
   render() {
     return (
       <div id="portfolio">
+        <div className="portfolio_title">
+          <p>
+            Case studies, videos and photo-shootings from some selected
+            projects.
+          </p>
+        </div>
         <div className="portfolio_grid_wrapper">
           {this.props.portfolioData &&
             this.props.portfolioData.map(project => (
