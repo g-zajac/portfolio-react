3bb380c1752fdc688de3e93a27b6ea32fbfceaea
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-26 21:54:31 +0200

    re-style portfolio

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -7,40 +7,70 @@ const { scaleDown } = transitions;
 export class Test extends React.Component {
   render() {
     return (
-      <div className="portfolio_grid_wrapper">
-        <StackGrid
-          appear={scaleDown.appear}
-          appeared={scaleDown.appeared}
-          enter={scaleDown.enter}
-          entered={scaleDown.entered}
-          leaved={scaleDown.leaved}
-          columnWidth={200}
-        >
-          <div key="key1">
-            <PortfolioThumbnail data={this.props.portfolioData[0]} />
-          </div>
-          <div key="key2">
-            <PortfolioThumbnail data={this.props.portfolioData[1]} />
-          </div>
-          <div key="key3">
-            <PortfolioThumbnail data={this.props.portfolioData[2]} />
-          </div>
-          <div key="key4">
-            <PortfolioThumbnail data={this.props.portfolioData[3]} />
-          </div>
-          <div key="key5">
-            <PortfolioThumbnail data={this.props.portfolioData[4]} />
-          </div>
-          <div key="key6">
-            <PortfolioThumbnail data={this.props.portfolioData[5]} />
-          </div>
-          <div key="key7">
-            <PortfolioThumbnail data={this.props.portfolioData[6]} />
-          </div>
-          <div key="key8">
-            <PortfolioThumbnail data={this.props.portfolioData[7]} />
-          </div>
-        </StackGrid>
+      <div id="portfolio_container">
+        <div className="portfolio_grid_wrapper">
+          <StackGrid
+            monitorImagesLoaded={true}
+            horizontal={true}
+            gutterWidth={10}
+            gutterHeight={10}
+            appear={scaleDown.appear}
+            appeared={scaleDown.appeared}
+            enter={scaleDown.enter}
+            entered={scaleDown.entered}
+            leaved={scaleDown.leaved}
+            columnWidth={200}
+          >
+            <div key="key1">
+              <img
+                src={this.props.portfolioData[0].minature_image_url}
+                alt={this.props.portfolioData[0].minature_image_tag}
+              />
+            </div>
+            <div key="key2">
+              <img
+                src={this.props.portfolioData[1].minature_image_url}
+                alt={this.props.portfolioData[1].minature_image_tag}
+              />
+            </div>
+            <div key="key3">
+              <img
+                src={this.props.portfolioData[2].minature_image_url}
+                alt={this.props.portfolioData[2].minature_image_tag}
+              />
+            </div>
+            <div key="key4">
+              <img
+                src={this.props.portfolioData[3].minature_image_url}
+                alt={this.props.portfolioData[3].minature_image_tag}
+              />
+            </div>
+            <div key="key5">
+              <img
+                src={this.props.portfolioData[4].minature_image_url}
+                alt={this.props.portfolioData[4].minature_image_tag}
+              />
+            </div>
+            <div key="key6">
+              <img
+                src={this.props.portfolioData[5].minature_image_url}
+                alt={this.props.portfolioData[5].minature_image_tag}
+              />
+            </div>
+            <div key="key7">
+              <img
+                src={this.props.portfolioData[6].minature_image_url}
+                alt={this.props.portfolioData[6].minature_image_tag}
+              />
+            </div>
+            <div key="key8">
+              <img
+                src={this.props.portfolioData[7].minature_image_url}
+                alt={this.props.portfolioData[7].minature_image_tag}
+              />
+            </div>
+          </StackGrid>
+        </div>
       </div>
     );
   }
