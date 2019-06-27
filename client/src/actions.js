a6543ff86289bf3eb23699dd3724ecbc506649dd
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-27 17:18:18 +0200

    add humidity and co2 actions

--- client/src/actions.js
+++ client/src/actions.js
@@ -13,8 +13,24 @@ export function slider(value) {
 }
 
 export function temperature(value) {
+  console.log("received action temperature: ", value);
   return {
     type: "TEMPERATURE",
     temp: value
+  };
+}
+
+export function humidity(value) {
+  console.log("received action humidity: ", value);
+  return {
+    type: "HUMIDITY",
+    hum: value
+  };
+}
+
+export function co2(value) {
+  return {
+    type: "CO2",
+    co2: value
   };
 }
