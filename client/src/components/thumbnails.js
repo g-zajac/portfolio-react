456277f1d5e5f84479727d698b258ae0d9aa5d98
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-27 16:13:43 +0200

    add scroll animation for thumbnails

--- client/src/components/thumbnails.js
+++ client/src/components/thumbnails.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/animate.min.css";
 
 class PortfolioThumbnail extends React.Component {
   render() {
     let projectData = this.props.data;
     return (
-      <div className="portfolio_thumbnail">
-        <Link to={projectData.link}>
-          <div className="overlay_box">
-            <div className="portfolio_thumbnail_text">
-              <h3>{projectData.title}</h3>
-              <p>{projectData.short_description}</p>
+      <ScrollAnimation animateIn="fadeIn">
+        <div className="portfolio_thumbnail">
+          <Link to={projectData.link}>
+            <div className="overlay_box">
+              <div className="portfolio_thumbnail_text">
+                <h3>{projectData.title}</h3>
+                <p>{projectData.short_description}</p>
+              </div>
             </div>
-          </div>
-          <img
-            src={projectData.minature_image_url}
-            alt={projectData.minature_image_tag}
-            className="portfolio_thumbnail_image"
-          />
-        </Link>
-      </div>
+            <img
+              src={projectData.minature_image_url}
+              alt={projectData.minature_image_tag}
+              className="portfolio_thumbnail_image"
+            />
+          </Link>
+        </div>
+      </ScrollAnimation>
     );
   }
 }
