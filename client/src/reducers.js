095fc7fd9ccde76335e49b145fa0251e7ec8c886
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-27 17:19:44 +0200

    add humidity and co2 reducers

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -6,5 +6,14 @@ export default function reducer(state = {}, action) {
   if (action.type === "TEMPERATURE") {
     return { ...state, temperature: action.temp };
   }
+
+  if (action.type === "HUMIDITY") {
+    return { ...state, humidity: action.hum };
+  }
+
+  if (action.type === "CO2") {
+    return { ...state, co2: action.co2 };
+  }
+
   return state;
 }
