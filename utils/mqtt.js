2545e44ed69c14a2137a9b0781665067ce6beee9
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-27 12:15:27 +0200

    bridge mqtt with socketio - temperature

--- utils/mqtt.js
+++ utils/mqtt.js
@@ -1,5 +1,5 @@
 var mqtt = require("mqtt");
-
+var serverSocket = require("../server");
 //TODO put it ot env
 var secrets = require("../secrets.json");
 
@@ -18,6 +18,9 @@ client.on("connect", function() {
     client.subscribe("/floating/socket1/relay/0/set", function(err) {
         console.log(err);
     });
+    client.subscribe("/node/sensor/dht/temperature", function(err) {
+        console.log(err);
+    });
     setInterval(function() {
         client.publish("/interval/ping", "test");
     }, 3000);
@@ -32,6 +35,10 @@ client.on("message", function(topic, message) {
     // message is Buffer
     console.log(topic.toString());
     console.log(message.toString());
+    if (topic == "/node/sensor/dht/temperature") {
+        console.log("temperature: ", message.toString());
+        serverSocket.sendTemperature(message.toString());
+    }
     //TODO send feedback socket io to switch
     // client.end(); //terminating connection and script
 });
