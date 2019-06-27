5368b3dac632dcea04f8d1d2890b8b022ad06f81
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-27 12:14:13 +0200

    add temperature emit

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import * as io from "socket.io-client";
 
-import { toggleSwitch, slider } from "./actions";
+import { toggleSwitch, slider, temperature } from "./actions";
 
 // import socketIOClient from "socket.io-client";
 // let proxyServer = "localhost:5000";
@@ -10,7 +10,7 @@ export const init = store => {
   if (!socket) {
     // socket = socketIOClient(proxyServer);
     socket = io("http://localhost:5000").connect();
-    // socket.on("chatMessages", msgs => store.dispatch(toggleSwitch(msgs)));
+    socket.on("temperature", msgs => store.dispatch(temperature(msgs)));
     // socket.on("chatMessage", msg => store.dispatch(slider(msg)));
   }
   return socket;
