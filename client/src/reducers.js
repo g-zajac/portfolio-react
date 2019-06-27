a574d0839dbe5e9f1300e034c7bb48c22c3b55f9
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-27 12:13:43 +0200

    add temperature reducer

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -2,4 +2,9 @@ export default function reducer(state = {}, action) {
   if (action.type === "SHOW_PROJECT") {
     return { ...state, showProject: action.projectId };
   }
+
+  if (action.type === "TEMPERATURE") {
+    return { ...state, temperature: action.temp };
+  }
+  return state;
 }
