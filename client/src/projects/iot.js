30d9e0cc8515a8ff54e871f9f00e243cc889a555
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-06-27 12:13:25 +0200

    use redux to get to sensor values

--- client/src/projects/iot.js
+++ client/src/projects/iot.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Switch from "@material-ui/core/Switch";
 import Slider from "@material-ui/lab/Slider";
@@ -10,11 +11,12 @@ const useStyles = makeStyles({
   }
 });
 
-export function Iot() {
+function Iot(props) {
   const classes = useStyles();
   const [state, setState] = React.useState({
     checkedB: true
   });
+  const [sensor, setTemp] = React.useState({ temperature: 0 });
 
   const toggleHandleChange = name => event => {
     console.log("toggle value: ", event.target.checked);
@@ -29,30 +31,40 @@ export function Iot() {
     setValue(newValue);
   };
 
+  console.log("props in iot: ", props);
+
   return (
     <div className="project" id="iot">
       <Link to="/">
         <i className="far fa-times-circle fa-2x" />
       </Link>
-      <div className={classes.root}>
-        Socket
-        <Switch
-          checked={state.checkedB}
-          onChange={toggleHandleChange("checkedB")}
-          value="checkedB"
-          color="primary"
-          inputProps={{ "aria-label": "primary checkbox" }}
-        />
-        <Slider
-          defaultValue={30}
-          onChange={sliderHandleChange}
-          getAriaValueText={valuetext}
-          aria-labelledby="continuous-slider"
-          valueLabelDisplay="auto"
-          marks
-          min={0}
-          max={100}
-        />
+      <div id="iot_container">
+        <div className={classes.root}>
+          <div id="iot_socket">
+            <i className="fas fa-plug fa-2x" />
+            <Switch
+              checked={state.checkedB}
+              onChange={toggleHandleChange("checkedB")}
+              value="checkedB"
+              color="primary"
+              inputProps={{ "aria-label": "primary checkbox" }}
+            />
+          </div>
+          <div id="iot_bulb">
+            <i className="fas fa-lightbulb fa-2x" />
+            <Slider
+              defaultValue={30}
+              onChange={sliderHandleChange}
+              getAriaValueText={valuetext}
+              aria-labelledby="continuous-slider"
+              valueLabelDisplay="auto"
+              marks
+              min={0}
+              max={100}
+            />
+          </div>
+          <div>Temperature {props.temperature}</div>
+        </div>
       </div>
     </div>
   );
@@ -61,3 +73,12 @@ export function Iot() {
 function valuetext(value) {
   return `${value}°C`;
 }
+
+const mapStateToProps = state => {
+  console.log("state.temp:", state.temperature);
+  return {
+    temperature: state.temperature
+  };
+};
+
+export default connect(mapStateToProps)(Iot);
